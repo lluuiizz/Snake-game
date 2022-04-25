994f6d270fbf206d91e2c6bf35c93870ed750d32
Luiz Guilherme Costa da Silva <luiz.developer.acc@gmail.com>
2022-04-25 16:24:56 -0300

    fruits, collision, things...

--- src/snake.c
+++ src/snake.c
@@ -8,8 +8,8 @@
 #include <stdbool.h>
 
 #define SNAKE_CHAR '#'
-#define INITIAL_SNAKE_LENGTH 1
-#define SIDE_SIZE 40
+#define INITIAL_SNAKE_LENGTH 5
+#define SIDE_SIZE 30
 #define STDIN 0     // INPUT FILE DESCRIPTOR
 #define TIME_SEC 0
 #define TIME_USEC 265 * 1000    //250
@@ -21,14 +21,14 @@ typedef struct snake_t {
 
 snake_t *snake_head = NULL;
 snake_t *snake_tail = NULL;
+unsigned int snake_size = 0;
 
 typedef struct fruit_t {
-    bool is_on_board;
+    bool put_fruit_on_board;
     unsigned int pos_x, pos_y;
 } fruit_t;
 
 fruit_t *fruit = NULL;
-
 
 char board[SIDE_SIZE][SIDE_SIZE];
 
@@ -36,13 +36,15 @@ void new_cell (char);
 void new_fruit ();
 void initialize_board();
 void update_snake(char, unsigned int *);
-void updateBoard();
-void keyboardListener(char *);
-void printBoardOnScreen();
-unsigned int genNumInBoard();
+void update_board_state();
+bool check_snake_body_collision();
+bool check_win();
+void keyboard_listener(char *);
+void print_board_on_screen();
+unsigned int gen_num_in_board();
 
 int main (void) {
-    char snakeMoveDirection = 's';
+    char snake_move_direction = 's';
     
     // ENABLE NON BLOCKING INPUT BY TURNING OFF ECHO AND ICANON 
     static struct termios oldt, newt;
@@ -53,39 +55,56 @@ int main (void) {
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
     
     
-    for (int i = 0; i < INITIAL_SNAKE_LENGTH; i++) {
-        new_cell(snakeMoveDirection);
+    for (int i = 1; i < INITIAL_SNAKE_LENGTH; i++) {
+        new_cell(snake_move_direction);
     }
     fruit = (fruit_t*) malloc(sizeof(fruit_t));
-    new_fruit();
     initialize_board();
+    new_fruit();
     unsigned int fruits_eaten_count = 0;
     //new_fruit();
     //updateBoard(gameBoard); 
     while(true) {
         system("clear");
-        printf("\t\t\t\t WHATEVER GAME\n\t\t\t\t FRUITS EATEN: %u\n", fruits_eaten_count);
-        update_snake(snakeMoveDirection, &fruits_eaten_count);
-        updateBoard();
-        printBoardOnScreen();
-        keyboardListener(&snakeMoveDirection); 
-    }    
+        printf("\t\t\t SNAKE GAME\n\t\t\t FRUITS EATEN: %u\n", fruits_eaten_count);
+        update_snake(snake_move_direction, &fruits_eaten_count);
+        update_board_state();
+        print_board_on_screen();
+        keyboard_listener(&snake_move_direction); 
+        
+        if (fruit->put_fruit_on_board) {
+            new_fruit();
 
+        }
+
+        if (check_snake_body_collision()) {
+            printf("\t\tYour body collided, sorry! :(");
+            printf("\n\t\tPress any key to exit: ");
+            break;
+        }
+       if (check_win()) {
+            printf("\t\tCongratulations, You Won the game!!!"); 
+            printf("\n\t\tPress any key to exit: ");
+            break;
+        }
+    }    
+    getchar();
+    printf("\n");
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // RETURN TO THE DEFAULT OPTIONS AFTER EXECUTION
     return 0;
 }
 
-void new_cell (char snakeMoveDirection) {
+void new_cell (char snake_move_direction) {
     
     if (snake_head == NULL) {
         snake_head = (snake_t*) malloc(sizeof(snake_t));
-        snake_head->pos_y = 10;
-        snake_head->pos_x = 10; 
+        snake_head->pos_y = SIDE_SIZE / 2 - 1;
+        snake_head->pos_x = SIDE_SIZE / 2 - 1; 
         snake_tail = snake_head;
     } else { 
         
         snake_t *new_cell = (snake_t*) malloc(sizeof(snake_t));
-        switch (snakeMoveDirection) {
+        switch (snake_move_direction) {
             case 'w':
                 new_cell->pos_x = snake_tail->pos_x;
                 new_cell->pos_y = snake_tail->pos_y + 1;
@@ -109,12 +128,17 @@ void new_cell (char snakeMoveDirection) {
 
         snake_tail = new_cell;
    }
+    snake_size += 1;
 }
 
 void new_fruit () {
-    fruit->pos_x = genNumInBoard();
-    fruit->pos_y = genNumInBoard();
-    fruit->is_on_board = false;
+    while (board[fruit->pos_y][fruit->pos_x] != ' ') {
+        fruit->pos_x = gen_num_in_board();
+        fruit->pos_y = gen_num_in_board();
+    }
+   
+    board[fruit->pos_y][fruit->pos_x] = '*';
+    fruit->put_fruit_on_board = false;
 
 }
 
@@ -130,7 +154,7 @@ void initialize_board() {
 	}
 }
 
-void update_snake (char snakeMoveDirection, unsigned int *fruits_eaten_count) {
+void update_snake (char snake_move_direction, unsigned int *fruits_eaten_count) {
     snake_t *head_buff = snake_head;
     snake_t *tail_buff = snake_tail;
     
@@ -143,7 +167,7 @@ void update_snake (char snakeMoveDirection, unsigned int *fruits_eaten_count) {
     
     }
     
-    switch(snakeMoveDirection) {
+    switch(snake_move_direction) {
         case 'w':
             snake_head->pos_y = (snake_head->pos_y-- > 1)
             ? snake_head->pos_y-- : SIDE_SIZE - 2;
@@ -165,13 +189,13 @@ void update_snake (char snakeMoveDirection, unsigned int *fruits_eaten_count) {
     snake_tail = tail_buff;
 
     if (snake_head->pos_x == fruit->pos_x && snake_head->pos_y == fruit->pos_y) {
-        new_fruit();
-        new_cell(snakeMoveDirection);
+        new_cell(snake_move_direction);
+        fruit->put_fruit_on_board = true;
         *fruits_eaten_count += 1;
     }
 }
 
-void updateBoard() {
+void update_board_state() {
     snake_t *head_buff = snake_head;
     
     while (snake_head != NULL) {
@@ -180,14 +204,30 @@ void updateBoard() {
     }
 
     snake_head = head_buff;
-
-    if (!fruit->is_on_board) {
-        board[fruit->pos_y][fruit->pos_x] = '*';
-        fruit->is_on_board = true;  
-    }
 } 
 
-void keyboardListener (char *currentDirection) {
+bool check_snake_body_collision() {
+    snake_t *body_cell = snake_head->next_body_cell;
+    bool collide = false;
+    while (body_cell != NULL) {
+        if (snake_head->pos_x == body_cell->pos_x && snake_head->pos_y == body_cell->pos_y) {
+            collide = true;
+            break;
+        }   
+       body_cell = body_cell->next_body_cell; 
+    }
+    return collide;
+
+}
+
+bool check_win() {
+    if (snake_size == (SIDE_SIZE * SIDE_SIZE) - (4 * SIDE_SIZE - 4)) {
+        return true;
+    }
+    return false;
+
+}
+void keyboard_listener (char *current_direction) {
     fd_set input;
     int fds, ret_val, num;
     struct timeval tv;
@@ -213,21 +253,19 @@ void keyboardListener (char *currentDirection) {
         return;
     }
     //  Verify inputed key
-    if ((ch == 'w' && *currentDirection != 's') || (ch == 's' && *currentDirection != 'w')
-    || (ch == 'd' && *currentDirection != 'a') || (ch == 'a' && *currentDirection != 'd')) {
-        *currentDirection = ch;
+    if ((ch == 'w' && *current_direction != 's') || (ch == 's' && *current_direction != 'w')
+    || (ch == 'd' && *current_direction != 'a') || (ch == 'a' && *current_direction != 'd')) {
+        *current_direction = ch;
     }
     
 }
-unsigned int genNumInBoard() {
+unsigned int gen_num_in_board() {
     unsigned int numValue = rand() % SIDE_SIZE;
-    while (numValue == 0 || numValue == SIDE_SIZE - 1)
-        numValue = rand() % SIDE_SIZE;
-    
+    srand(time(0)); 
     return numValue;
 }
 
-void printBoardOnScreen() {
+void print_board_on_screen() {
 	for (unsigned int y = 0; y < SIDE_SIZE; y++) {
 		for (unsigned int x = 0; x < SIDE_SIZE; x++) {
 			printf("%c ", board[y][x]);
